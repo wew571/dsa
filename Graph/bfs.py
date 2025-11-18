a178d0cec912e614482107be719f82376722d15c
wew57 <huy1872006k@gmail.com>
2025-11-18 13:07:25 +0700

    Implement BFS algorithm in bfs.py

---
+++ Graph/bfs.py
@@ -0,0 +1,36 @@
+from collections import deque
+import Romania
+
+def bfs(graph , start, goal):
+    node_path = {}
+    visited = set()
+    queue = deque()
+    queue.append(start)
+
+    while queue:
+        concurrent = queue.popleft()
+        if concurrent == goal:
+            path = reconstructPath(node_path, start, goal)
+            return path
+
+        for neighbor  in graph[concurrent]['neighbors']:
+            if neighbor not in visited:
+                node_path[neighbor] = concurrent
+                visited.add(neighbor)
+                queue.append(neighbor)
+    return None
+
+def t():
+    graph = Romania.getGraphRomania()
+    start = 'Arad'
+    goal = 'Bucharest'
+
+    path = bfs(graph, start, goal)
+
+    if path:
+        print("Path found:", " -> ".join(path))
+    else:
+        print("No path found")
+
+
+t()
